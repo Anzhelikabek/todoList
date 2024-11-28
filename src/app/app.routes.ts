f26bc9abdb8a9d064b182621a537d004f8047708
Anzhelikabek <anzhelika.beksultanova@gmail.com>
2024-11-28 16:43:12 +0600

    first

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './pages/main/main.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // { path: '', redirectTo: 'main', pathMatch: 'full' }, // Перенаправление на главную страницу
+  { path: '', component: MainComponent }, // Основной маршрут для MainComponent
+];
